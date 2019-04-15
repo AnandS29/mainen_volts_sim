4c4df40bf8fb0f113ee6866563bb33f652d30c5b
Anand Siththaranjan <anandsranjan@berkeley.edu>
2019-04-15 10:46:59 -0700

    Num of files to gen change - 10v26 ext
    Changed from 400 files to 2000 files

--- setup10v26_ext/make_volts.sh
+++ setup10v26_ext/make_volts.sh
@@ -1,4 +1,4 @@
 module load tensorflow/intel-1.12.0-py36
 module load python/3.6-anaconda-4.4
-python ./paramsetGen.py --num=400 --model "mainen10v26" --vary 1 2 3 4 5 6 7 8 13 14 --start=2001
+python ./paramsetGen.py --num=2000 --model "mainen10v26" --vary 1 2 3 4 5 6 7 8 13 14 --start=2001
 sbatch sbatch_run.slr
