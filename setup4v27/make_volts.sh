0159be65630baea7575384714177b45828e4ea9c
Anand Siththaranjan <anandsranjan@berkeley.edu>
2019-04-15 16:27:23 -0700

    Changed ./ to ../common/

--- setup4v27/make_volts.sh
+++ setup4v27/make_volts.sh
@@ -1,4 +1,4 @@
 module load tensorflow/intel-1.12.0-py36
 module load python/3.6-anaconda-4.4
-python ./paramsetGen.py --num=100 --model "mainen4v27" --vary 2 4 7 8
+python ../common/paramsetGen.py --num=100 --model "mainen4v27" --vary 2 4 7 8
 sbatch sbatch_run.slr
