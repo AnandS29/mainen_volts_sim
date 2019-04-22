bda9c8d06f781ed0e6acb6d7f638d3e9a37b03eb
Anand Siththaranjan <anandsranjan@berkeley.edu>
2019-04-22 00:34:57 -0700

    setup for 7v31 and 10v32

---
+++ setup7v31/make_volts.sh
@@ -0,0 +1,18 @@
+module load tensorflow/intel-1.12.0-py36
+module load python/3.6-anaconda-4.4
+
+c='mainen7v31'
+stimName='chirp23a'
+coreN=${c}
+wrkDir=${SCRATCH}/${coreN}
+mkdir -p wrkDir
+mkdir -p ${wrkDir}/'data'
+mkdir -p ${wrkDir}/'volts'
+mkdir -p ${wrkDir}/'stims'
+mkdir -p ${wrkDir}/'params'
+
+cp mainen_params_wide_range.csv ${wrkDir}/'params'
+cp /global/homes/a/asranjan/ML/run_mainen/${stimName}/${stimName}.csv ${wrkDir}/'stims'
+
+python ../common/paramsetGen.py --num=100 --model "mainen7v31" --vary 2 4 5 6 7 8 14 --seed=1846353
+sbatch sbatch_run.slr
