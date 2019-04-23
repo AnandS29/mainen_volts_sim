54521867f139564fa8b7e6d01aaefaa4f323b459
Anand Siththaranjan <anandsranjan@berkeley.edu>
2019-04-22 17:04:55 -0700

    Added ext files for 10v32

---
+++ setup10v32/make_volts_ext1.sh
@@ -0,0 +1,18 @@
+module load tensorflow/intel-1.12.0-py36
+module load python/3.6-anaconda-4.4
+
+c='mainen10v32'
+stimName='chirp23a'
+coreN=${c}
+wrkDir=${SCRATCH}/${coreN}
+mkdir -p wrkDir
+mkdir -p ${wrkDir}/'data'
+mkdir -p ${wrkDir}/'volts'
+mkdir -p ${wrkDir}/'stims'
+mkdir -p ${wrkDir}/'params'
+
+cp mainen_params_wide_range.csv ${wrkDir}/'params'
+cp /global/homes/a/asranjan/ML/run_mainen/${stimName}/${stimName}.csv ${wrkDir}/'stims'
+
+python ../common/paramsetGen.py --num=3810 --model "mainen10v32" --vary 1 2 3 4 5 6 7 8 13 14 --seed=1846352 --start=101
+sbatch sbatch_run.slr
