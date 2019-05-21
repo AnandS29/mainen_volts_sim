106a56de8b5bf4aa3f02a18e936f31a217dc6cd1
Anand Siththaranjan <anandsranjan@berkeley.edu>
2019-05-21 16:55:06 -0700

    Adapted for multiple stims + changed common hoc file (times file)

--- common/multi_model/run_one_pin_parts.py
+++ common/multi_model/run_one_pin_parts.py
@@ -22,10 +22,10 @@ def load_and_run(run_file, stim_file, times_file, out_file, params_file):
 #             print(volts)
 
 run_file = './run_model_cori_pin_parts.hoc'
-times = './times_0.02_23k.csv'
+times = './times_0.02_10k.csv'
 #stims_path = './chirp23a/'
 stims_path = './stims/'
-volts_path =  '/global/cscratch1/sd/asranjan/multi_test_6/volts/'
+volts_path =  '/global/cscratch1/sd/asranjan/sample_10a/volts/'
 stims_name_list = os.listdir(stims_path)
 for stim in stims_name_list:
 	load_and_run(run_file, stims_path + stim, times, volts_path + 'pin_512_' + str(i) + "_" + stim, str(i) + ".csv")
--- sample_10a/make_volts.sh
+++ sample_10a/make_volts.sh
@@ -1,7 +1,7 @@
 module load tensorflow/intel-1.12.0-py36
 module load python/3.6-anaconda-4.4
 
-c='multi_test_6'
+c='sample_10a'
 #stimName='chirp23a'
 stimName='stims'
 coreN=${c}
@@ -15,5 +15,5 @@ mkdir -p ${wrkDir}/'params'
 cp mainen_params_wide_range.csv ${wrkDir}/'params'
 cp -R /global/homes/a/asranjan/ML/run_mainen/${stimName}/. ${wrkDir}/'stims'
 
-python ../common/paramsetGen.py --num=2 --model "multi_test_6" --vary 2 4 7 8 --seed=1846353
+python ../common/paramsetGen.py --num=2 --model "sample_10a" --vary 2 4 7 8 --seed=1846353
 sbatch sbatch_run.slr
